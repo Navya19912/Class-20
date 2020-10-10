7c5b6b74b2bbe77dfdb09fb2cc5954539acabe96
Navya19912 <72553049+Navya19912@users.noreply.github.com>
2020-10-10 16:48:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var fixed, moving;
+
+
+
+function setup() {
+  createCanvas(800,400);
+  fixed= createSprite(200, 200, 50, 80);
+  fixed.shapeColor="red";
+
+  moving=createSprite(600,200,50,80);
+  moving.shapeColor="white";
+  
+}
+
+function draw() {
+  background("yellow");  
+
+  moving.x=World.mouseX;
+  moving.y=World.mouseY;
+
+if(moving.x-fixed.x < fixed.width/2+ moving.width/2
+   && fixed.x-moving.x < fixed.width/2 + moving.width/2
+   && moving.y-fixed.y < fixed.height/2+ moving.height/2
+   && fixed.y-moving.y < fixed.height/2 + moving.height/2 )
+   {
+     moving.shapeColor="green";
+     fixed.shapeColor="green";
+   }
+else {
+  moving.shapeColor="white";
+  fixed.shapeColor="red";
+}
+
+  drawSprites();
+}
